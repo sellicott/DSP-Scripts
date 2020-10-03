c5d4717b8e4ebee48eadcb257065472fae60d2c3
Sam Ellicott <sellicott@cedarville.edu>
2020-10-02 23:43:22 -0400

    Test program

--- C/plot-test.c
+++ C/plot-test.c
@@ -5,10 +5,8 @@
  * date modified: 08-23-20
  */
 
-// so the compiler doesn't complain at us
-#define _POSIX_C_SOURCE 2
-
 #include <stdio.h>
+#include <math.h>
 
 #include "gnuplotlib.h"
 
@@ -18,10 +16,28 @@ int main(void)
 {
   gnuplot_handle_t* gnuplot = gnuplot_open();
 
+
+  const size_t num_elem = 100;
+  const float pi = 3.141592654;
+  float x[100] = {0};
+  float y1[100] = {0};
+  float y2[100] = {0};
+
+  for (int i = 0; i < num_elem; i++) {
+    x[i] = 2.0f * pi * i / (float) num_elem;
+    y1[i] = sinf(x[i]);
+    y2[i] = cosf(x[i]);
+  }
+
+  gnuplot_figure_t* fig1 = gnuplot_newfigure(gnuplot);
+  gnuplot_plot2d_line(gnuplot, fig1, x, num_elem, y1, num_elem);
+  gnuplot_newfigure(gnuplot);
+  gnuplot_plot2d_line(gnuplot, NULL, x, num_elem, y2, num_elem);
+  gnuplot_hold_on(gnuplot, fig1);
+
   puts("Press enter to exit\n");
   getc(stdin);
+  gnuplot_close(gnuplot);
 
-  //fputs("EOF\n", gnuplot);
-  pclose(gnuplot);
   return 0;
 }
